21cf076ef21646e62654f0aff6bbfc6251a6131b
Charles Ming <charlesming@berkeley.edu>
2022-09-30 14:38:26 -0700

    add theming; create sample button

--- screens/Draft.tsx
+++ screens/Draft.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
 import { Button } from "react-native-elements";
 import { getAuth, signOut } from "firebase/auth";
+import theme from "../styles/theme";
 
 const auth = getAuth();
 
@@ -12,6 +13,9 @@ const DraftScreen = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <Text>Job post drafting</Text>
+      <TouchableOpacity style={styles.postButton}>
+        <Text style={styles.buttonText}>Post</Text>
+      </TouchableOpacity>
       <Button
         title="Back"
         style={styles.button}
@@ -31,6 +35,14 @@ const styles = StyleSheet.create({
   button: {
     marginTop: 10,
   },
+  postButton: {
+    ...theme.buttons.feedStandard,
+    backgroundColor: theme.colors.cardBackground
+  },
+  buttonText: {
+    ...theme.textVariants.body,
+    alignSelf: "center"
+  }
 });
 
 export default DraftScreen;
