9e17e528ae1da0e033cca665d72b2d0a850a98d9
annie <annie@annies-MacBook-Pro-2.local>
2022-10-02 16:57:13 -0700

    rough signin styling

--- screens/Authentication/Signin.tsx
+++ screens/Authentication/Signin.tsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useAuthentication } from "../utils/hooks/useAuthentication";
-import { Button } from "react-native-elements";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useAuthentication } from "../../utils/hooks/useAuthentication";
+import { Pressable } from "react-native";
 import { getAuth, signOut } from "firebase/auth";
 
 const auth = getAuth();
+
+const logo = require('../../assets/favicon.png');
 
 const SigninScreen = ({ navigation }: any) => {
   const { user } = useAuthentication();
 
   return (
+
+    <View style={styles.logocontainer} >
+    <Image source={logo} style={styles.logo}/>
     <View style={styles.container}>
-      <Text>Sign in</Text>
-      <Button
-        title="Back"
+      
+      <Text style={styles.text1}>Welcome to the Chinese Newcomers Service Center job portal!</Text>
+      <Text style={styles.text2}>Let's get your account set up.</Text>
+     
+      <Pressable
         style={styles.button}
         onPress={() => navigation.navigate("Home")}
-      />
+      >
+        <Text style={styles.text3}> SIGN UP </Text>
+      </Pressable>
+      
+      
+
+    </View>
     </View>
   );
 };
@@ -24,13 +37,44 @@ const SigninScreen = ({ navigation }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ff",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    
+  },
+  logocontainer: {
+    height:"100%",
+    marginLeft: "10%",
+    marginTop: "10%",
+  },
+  logo: {
+    resizeMode: "contain",
+    height: "17%",
+    width: "17%",
+  },
+  button: {
+    backgroundColor: "#E13C3C",
+    width: "80%",
+    height: "6.5%",
+    borderRadius: 5,
     alignItems: "center",
     justifyContent: "center",
   },
-  button: {
-    marginTop: 10,
+  text1: {
+    color: 'black',
+    fontSize: 30, 
   },
+  text2: {
+    color: '#797979',
+    fontSize: 15,   
+  },
+  text3: {
+    color: 'white',
+    fontSize: 25, 
+
+  },
+  
+  
 });
 
 export default SigninScreen;
