d39e72d5519cce536e419dc8c1855ed506f40b0d
Mia Chou <Miachou@berkeley.edu>
2022-10-04 17:54:23 -0700

    got a modal to show up heck yeah

--- screens/Feed/JobCard.tsx
+++ screens/Feed/JobCard.tsx
@@ -1,10 +1,30 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, Modal } from "react-native";
 import styles from './CardStyles'
-import * as React from "react";
+import React, {useState} from "react";
 
 const JobCard = ({title, description}:{title: string; description: string}) => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.cardContainer}>
+
+    <Modal
+        transparent={true}
+        visible={modalVisible}
+        animationType = "slide"
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+          </View>
+        </View>
+      </Modal>
+
       <View style={styles.cardHeader}>
         <Text>{title}</Text>
       </View>
@@ -12,12 +32,13 @@ const JobCard = ({title, description}:{title: string; description: string}) => {
           <Text>{description}</Text>
       </View>
       <View style={styles.cardFooter}>
-        <Pressable style={styles.moreInfoButton}> 
-        <Text>More Info</Text> 
+
+        <Pressable style={styles.moreInfoButton} onPress={() => {
+          setModalVisible(true)}}> 
+        <Text >More Inforr</Text> 
         </Pressable>
       </View>
     </View> 
-  );
-};
+  );}
 
 export default JobCard;
