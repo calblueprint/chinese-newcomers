5fd3efd64c624ab7aa42f1106bf53760b67093ef
annie <annie@annies-MacBook-Pro-2.local>
2022-11-17 02:00:50 -0800

    linked admin auth object to firestore user object

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -5,6 +5,7 @@ import {
   signOut,
   createUserWithEmailAndPassword
 } from 'firebase/auth';
+import { getUser, addUser } from '../firebase/firestore/user';
 
 const auth = getAuth();
 // TODO: CHANGE 'recaptcha-container' TO ID OF CAPTCHA CONTAINER
@@ -49,7 +50,28 @@ export const registerWithEmailAndPassword = async (
 ): Promise<User> => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    return userCredential;
+    const user = userCredential.user;
+    console.log(user);
+    const userObject = await getUser(user.uid);
+    if (userObject !== null) {
+      console.log('Got user from users collection. Name: ' + userObject.name);
+      // TODO: probably put user object into react context
+    } else {
+      console.log('Create new user flow');
+      // TODO: handle user not yet in users collection. check access collection to see what type of user to create
+      // below code just for testing
+      await addUser({
+        id: user.uid,
+        access: 'admin',
+        createdJobs: [],
+        email: user.email,
+        likedJobs: [], // switched to string of jobIds to match Firebase
+        name: 'test phone',
+        phoneNumber: user.phoneNumber,
+        verified: true,
+        password: null
+      });
+    }
   } catch (e) {
     console.error(e);
     throw e;
--- src/firebase/firestore/adminuser.ts
+++
@@ -1,3 +0,0 @@
-import { firebaseApp, db } from '../firebaseApp';
-import { User } from '../../types/types';
-import { addDoc, collection, deleteDoc, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
