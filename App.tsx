50d5333d3b782abbc5ca7e85572779525a6a6539
Charles Ming <70979542+CharlesMing02@users.noreply.github.com>
2022-12-02 16:13:05 -0800

    create modal to confirm job post created, import fonts (#33)
    * create modal to confirm job post created
    
    * import custom fonts globally, improve font styling

--- App.tsx
+++ App.tsx
@@ -4,12 +4,24 @@ import { UserProvider } from './src/context/context';
 import './src/firebase/firebaseApp';
 import RootNavigation from './src/navigation';
 
+import { useFonts } from 'expo-font';
+
 export default function App() {
-  return (
-    <ThemeProvider>
-      <UserProvider>
-        <RootNavigation />
-      </UserProvider>
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    "DMSans_700Bold": require('./src/assets/DMSans-Bold.ttf'),
+    "DMSans_500Medium": require('./src/assets/DMSans-Medium.ttf'),
+    "DMSans_400Regular": require('./src/assets/DMSans-Regular.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    return (
+      <ThemeProvider>
+        <UserProvider>
+          <RootNavigation />
+        </UserProvider>
+      </ThemeProvider>
+    );
+  }
 }
