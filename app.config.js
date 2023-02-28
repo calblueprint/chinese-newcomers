fa967bac7501f5666672a7ed9ed719873b4d50c2
Suraj Rao <suraj_rao@berkeley.edu>
2023-02-27 16:37:24 -0800

    [build works] change useFrameworks to static, use newArchEnabled in app.config.js

--- app.config.js
+++ app.config.js
@@ -54,7 +54,8 @@ export default {
           },
           ios: {
             deploymentTarget: '13.0',
-            useFrameworks: 'dynamic'
+            useFrameworks: 'static',
+            newArchEnabled: true
           }
         }
       ]
