094c130b75714d4d0127e6abfdc6e08fc7b495c9
Celine Choi <celinejwchoi@berkeley.edu>
2023-03-11 15:46:12 -0800

    [feat] rewrote functions to handle different user types

--- src/firebase/firestore/user.ts
+++ src/firebase/firestore/user.ts
@@ -9,135 +9,80 @@ import {
 } from 'firebase/firestore';
 import { UserCredential } from 'firebase/auth';
 import { db } from '../config';
-import { RegularUser, Admin, Employer } from '../../types/types';
+import { RegularUser, Admin, Employer, genericUser } from '../../types/types';
 
 interface Dictionary<T> {
-  [key: string]: T;
+  [key: genericUser]: T;
 }
 
-const parseRegularUser = async (document: QueryDocumentSnapshot<DocumentData>) => {
+// QueryDocumentSnapshot<DocumentData>
+const parseUser = async (document: QueryDocumentSnapshot<any>) => {
   const userId = document.id.toString();
   const data = document.data();
-  const user = {
-    id: userId,
-    access: data.access,
-    createdJobs: data.createdJobs, // might need to map to job objects later
-    email: data.email,
-    likedJobs: data.likedJobs, // might need to map to job objects later
-    name: data.name,
-    phoneNumber: data.phoneNumber,
-    verified: data.verified,
-    password: data.password,
-  };
-  return user as RegularUser;
-};
-
-const parseEmployer = async (document: QueryDocumentSnapshot<DocumentData>) => {
-  const userId = document.id.toString();
-  const data = document.data();
-  const user = {
-    id: userId,
-    access: data.access,
-    createdJobs: data.createdJobs, // might need to map to job objects later
-    email: data.email,
-    name: data.name,
-    phoneNumber: data.phoneNumber,
-    verified: data.verified,
-    password: data.password,
-  };
-  return user as Employer;
-};
-
-const parseAdmin = async (document: QueryDocumentSnapshot<DocumentData>) => {
-  const userId = document.id.toString();
-  const data = document.data();
-  const user = {
-    id: userId,
-    access: data.access,
-    createdJobs: data.createdJobs, // might need to map to job objects later
-    email: data.email,
-    name: data.name,
-    phoneNumber: data.phoneNumber,
-    verified: data.verified,
-    password: data.password,
-  };
-  return user as Admin;
-};
-
-export const getRegularUser = async (id: string): Promise<RegularUser | null> => {
-  const docRef = doc(db, 'regularUser', id);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    console.log('User data:', docSnap.data());
-    return parseRegularUser(docSnap);
+  const type = data.access;
+  
+  const userConstructors: Dictionary<typeof genericUser> = {
+    regular: RegularUser,
+    admin: Admin,
+    employer: Employer,
   }
-  // doc.data() will be undefined in this case
-  console.log('No such document!');
-  return null;
-};
 
-export const getAdmin = async (id: string): Promise<Admin | null> => {
-  const docRef = doc(db, 'admin', id);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    console.log('User data:', docSnap.data());
-    return parseAdmin(docSnap);
+  const userType = userConstructors[type];
+
+  const user = {
+    userId,
+    ...data,
   }
-  // doc.data() will be undefined in this case
-  console.log('No such document!');
-  return null;
+
+  return user as typeof userType;
 };
 
-export const getEmployer = async (id: string): Promise<Employer | null> => {
-  const docRef = doc(db, 'employer', id);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    console.log('User data:', docSnap.data());
-    return parseEmployer(docSnap);
-  }
-  // doc.data() will be undefined in this case
-  console.log('No such document!');
-  return null;
+export const getUser = async (id: string): Promise<genericUser | null> => {
+  const userRefs = [
+    doc(db, 'regularUser', id),
+    doc(db, 'admin', id),
+    doc(db, 'employer', id),
+  ];
+
+  const userDocs = await Promise.all(userRefs.map((ref) => getDoc(ref)));
+  const userDoc = userDocs.find((doc) => doc.exists);
+  if (!userDoc) {
+      console.log('No such document!');
+      return null;
+  };
+  console.log('User data:', userDoc.data());
+  return parseUser(userDoc);
 };
 
-export const addRegularUser = async (user: RegularUser): Promise<void> => {
-  const itemsRef = doc(db, 'regularUser', user.id);
+export const addUser = async (user: genericUser): Promise<void> => {
+  const type = user.access;
+  const itemsRef = doc(db, type, user.id);
   await setDoc(itemsRef, user);
 };
 
-export const addEmployer = async (user: Employer): Promise<void> => {
-  const itemsRef = doc(db, 'employer', user.id);
-  await setDoc(itemsRef, user);
-};
+export const updateUser = async(userId: string, newField: Dictionary<string>) => {
+  const user = getUser(userId);
+  const userConstructors: Dictionary<string> = {
+    RegularUser: 'regularUser',
+    Admin: 'admin',
+    Employer: 'employer',
+  };
 
-export const addAdmin = async (user: Admin): Promise<void> => {
-  const itemsRef = doc(db, 'admin', user.id);
-  await setDoc(itemsRef, user);
-};
-
-export const updateRegularUser = async (userId: string, newField: Dictionary<string>): Promise<void> => {
-  const docRef = doc(db, 'regularUser', userId);
+  const userType = userConstructors[user];
+  const docRef = doc(db, userType, userId);
   const field = Object.keys(newField)[0];
   const data = {
     field = newField[field];
-  }
+  };
   await updateDoc(docRef, data);
+};
+
+export const deleteUser = async (user: genericUser): Promise<void> => {
+  const type = user.access;
+  const userId = user.id;
+  const docRef = doc(db, type, userId);
+  await deleteDoc(docRef);
 }
-
-export const deleteRegularUser = async (userId: string): Promise<void> => {
-  const docRef = doc(db, 'regularUser', userId);
-  await deleteDoc(docRef);
-};
-
-export const deleteAdmin = async (userId: string): Promise<void> => {
-  const docRef = doc(db, 'admin', userId);
-  await deleteDoc(docRef);
-};
-
-export const deleteEmployer = async (userId: string): Promise<void> => {
-  const docRef = doc(db, 'employer', userId);
-  await deleteDoc(docRef);
-};
 
 
 export const checkAndAddUser = async (
