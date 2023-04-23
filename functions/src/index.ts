16f84d266ff2c3454371a78e9664182d5378f8eb
Stephanie Wong <stwong2@berkeley.edu>
2023-04-23 13:05:28 -0700

    function w logs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,14 +9,17 @@ const approvedJobsCollection = firestore.collection('approvedJobs');
 exports.updateExpiredJobs = functions.pubsub
   .schedule('0 12 * * *') // 0 12 * * * is once a day
   .onRun(async () => {
+    functions.logger.log('at top of cloud function');
     const approvedJobs = approvedJobsCollection.get();
     const now = new Date();
     (await approvedJobs).forEach(async job => {
+      functions.logger.log('looping through jobs collection');
       const jobId = job.get('id');
       const jobDate = new Date(job.get('date').seconds * 1000);
       const diff = now.getTime() - jobDate.getTime();
       // 60 days in milliseconds --> 5184000000
       if (diff > 5184000000) {
+        functions.logger.log('removing a job inside cloud function');
         const testCloudFunctionCollection =
           firestore.collection('testCloudFunction');
         testCloudFunctionCollection.doc(jobId).set(job.data());
