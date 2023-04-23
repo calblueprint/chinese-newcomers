72baf0d9a3357fd40c1328ffeac7bbd686af5545
Casey Lei <63774760+caseyzlei@users.noreply.github.com>
2023-04-23 12:00:46 -0700

    made changeCreatedJobStatus function

--- src/firebase/firestore/employer.ts
+++ src/firebase/firestore/employer.ts
@@ -26,27 +26,27 @@ export const addCreatedJobs = async (
         throw e;
     };
 // Create function to remove single job (either notApproved or approvedJobs) from map of employerJobs
-export const removeCreatedJobs = async (
-    jobID: string,
-    // collectionName: string,
-    employerID: string,
-): Promise<void> => {
-    try {
-        const docRef = doc(db, ‘employer’, employerID);
-        const data = jobID;
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            if (docSnap.data().createdJobs.get(jobID)) {
-                await updateDoc(docRef, {notApprovedJobs: arrayRemove(data) });
-            } else {
-                await updateDoc(docRef, {approvedJobs: arrayRemove(data) });
-            }
-        }
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
-};
+// export const removeCreatedJobs = async (
+//     jobID: string,
+//     // collectionName: string,
+//     employerID: string,
+// ): Promise<void> => {
+//     try {
+//         const docRef = doc(db, ‘employer’, employerID);
+//         const data = jobID;
+//         const docSnap = await getDoc(docRef);
+//         if (docSnap.exists()) {
+//             if (docSnap.data().createdJobs.get(jobID)) {
+//                 await updateDoc(docRef, {notApprovedJobs: arrayRemove(data) });
+//             } else {
+//                 await updateDoc(docRef, {approvedJobs: arrayRemove(data) });
+//             }
+//         }
+//     } catch (e) {
+//         console.error(e);
+//         throw e;
+//     }
+// };
 // Create function to update createdJobs with map of employerJobs
 export const updateEmployerJobs = async (
   employerJobs: Map,
@@ -97,9 +97,21 @@ export const getAllCreatedJobs = async (
 // Change status of existing job in map
 // Use when job is approved to change from pending to approved
 export const changeCreatedJobsStatus = async (
-    id: string,
+    jobId: string,
     collectionName: string,
-): Promise<>
+): Promise<void> => {
+    try {
+        const docRef = doc(db, collectionName, jobId);
+        if (collectionName == 'notApprovedJobs') {
+            updateDoc(docRef, { collectionName: 'approvedJobs' });
+        } else {
+            console.log('This job is already approved.')
+        }
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+};
 
 // Build out screens, filtering between approved & pending (similar to feed screen)
 
