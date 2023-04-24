6f4183ea86c1fad371aa57fa3edc677011de8002
Stephanie Wong <stwong2@berkeley.edu>
2023-04-23 17:17:13 -0700

    fixed func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,7 @@ import admin = require('firebase-admin');
 admin.initializeApp();
 const firestore = admin.firestore();
 
-// const approvedJobsCollection = firestore.collection('approvedJobs');
-const approvedJobsCollection = firestore.collection('testCloudFunction');
+const approvedJobsCollection = firestore.collection('approvedJobs');
 
 exports.updateExpiredJobs = functions.pubsub
   .schedule('0 12 * * *')
@@ -13,14 +12,12 @@ exports.updateExpiredJobs = functions.pubsub
     const approvedJobs = approvedJobsCollection.get();
     const now = new Date();
     (await approvedJobs).forEach(async job => {
-      // const jobId = job.get('id');
+      const jobId = job.get('id');
       const jobDate = new Date(job.get('date').seconds * 1000);
       const diff = now.getTime() - jobDate.getTime();
-      // 259200000
-      // 5184000000
-      if (diff > 259200000) {
-        // const expiredJobsCollection = firestore.collection('expiredJobs');
-        // expiredJobsCollection.doc(jobId).set(job.data());
+      if (diff > 5184000000) {
+        const expiredJobsCollection = firestore.collection('expiredJobs');
+        expiredJobsCollection.doc(jobId).set(job.data());
         job.ref.delete();
       }
     });
