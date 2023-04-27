fec33dfe64d941cc441a1f71ce4bc0a045d4c6a3
alliu879 <alberliu@berkeley.edu>
2023-04-26 18:07:04 -0700

    add new constants for firestore collection names: (#84)

---
+++ src/firebase/firestore/constants.ts
@@ -0,0 +1,9 @@
+export const ACCESS_COLLECTION = "access";
+export const ADMIN_COLLECTION = "admin";
+export const APPROVED_JOBS_COLLECTION = "approvedJobs";
+export const EMPLOYER_COLLECTION = "employer";
+export const METADATA_COLLECTION = "metadata";
+export const REGULAR_USER_COLLECTION = "regularUser";
+export const TEST_CLOUD_FUNCTION_COLLECTION = "testCloudFunction";
+export const USER_TYPES_COLLECTION = "userTypes";
+export const USERS_COLLECTION = "users";
