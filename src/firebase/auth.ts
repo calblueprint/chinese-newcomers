bb5ccf0c4510adb1e779b56d91e563cdded54d13
Albert Liu <alberliu@berkeley.edu>
2023-04-29 15:49:11 -0700

    fix auth

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,12 +1,12 @@
 import {
   PhoneAuthProvider,
-  RegularUser,
   createUserWithEmailAndPassword,
   deleteUser,
   getAuth,
   signInWithCredential,
   signInWithEmailAndPassword,
   signOut,
+  User,
 } from 'firebase/auth';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { AuthDispatch } from '../context/AuthContext';
@@ -92,7 +92,7 @@ export const signUpPhoneAdmin = async (
     if (user == null) {
       console.warn('null user error'); // TODO: handle null user case
     }
-    deleteUser(user as RegularUser)
+    deleteUser(user as User)
       .then(() => {
         console.log('User successfully deleted');
       })
@@ -124,7 +124,6 @@ export const signUpEmail = async (
   dispatch: AuthDispatch,
   params: { email: string; password: string; phoneNumber: string },
 ) => {
-<<<<<<< HEAD
   createUserWithEmailAndPassword(auth, params.email, params.password)
     .then(async userCredential => {
       console.log("signup email");
@@ -138,26 +137,12 @@ export const signUpEmail = async (
     .catch(error => {
       console.warn('Email sign up error', error);
     });
-=======
-  const userCredential = await createUserWithEmailAndPassword(
-    auth,
-    params.email,
-    params.password,
-  );
-  const { user } = userCredential;
-  await checkAndAddUser(user, 'admin', params.phoneNumber);
-  console.log('Email sign up successful', user.email);
-  await activatedAdmin(params.phoneNumber);
-  const UserObject = await getUser(user.uid);
-  dispatch({ type: 'SIGN_IN', userObject: UserObject });
->>>>>>> main
 };
 
 export const signInEmail = async (
   dispatch: AuthDispatch,
   params: { email: string; password: string },
 ) => {
-<<<<<<< HEAD
   signInWithEmailAndPassword(auth, params.email, params.password)
     .then(async userCredential => {
       const { user } = userCredential;
@@ -167,27 +152,6 @@ export const signInEmail = async (
     .catch(error => {
       console.warn('Email sign in error', error);
     });
-=======
-  const userCredential = await signInWithEmailAndPassword(
-    auth, 
-    params.email, 
-    params.password
-  );
-  const { user } = userCredential;
-  console.log('Email sign in successful', user.email);
-  const UserObject = await getUser(user.uid);
-  dispatch({ type: 'SIGN_IN', userObject: UserObject });
-  // signInWithEmailAndPassword(auth, params.email, params.password)
-  //   .then(async userCredential => {
-  //     const { user } = userCredential;
-  //     console.log('Email sign in successful', user.email);
-  //     const UserObject = await getUser(user.uid);
-  //     dispatch({ type: 'SIGN_IN', userObject: UserObject });
-  //   })
-  //   .catch(error => {
-  //     console.warn('Email sign in error', error);
-  //   });
->>>>>>> main
 };
 
 export const signInPhone = async (
