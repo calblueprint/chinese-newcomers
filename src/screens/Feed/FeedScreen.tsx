1565bde6e3e6b28cb398293349f2394fa77cd740
annie <anniehuo@berkeley.edu>
2023-04-30 04:18:37 -0700

    merge conflicts

--- src/screens/Feed/FeedScreen.tsx
+++ src/screens/Feed/FeedScreen.tsx
@@ -7,7 +7,7 @@ import { AuthContext } from '../../context/AuthContext';
 import { updateUserBookmarks } from '../../firebase/firestore/user';
 import { FeedStackScreenProps } from '../../types/navigation';
 import { Job } from '../../types/types';
-import styles from './styles';
+import styles from './Styles';
 
 function FeedScreen({ navigation }: FeedStackScreenProps<'FeedScreen'>) {
   const { userObject } = useContext(AuthContext);
